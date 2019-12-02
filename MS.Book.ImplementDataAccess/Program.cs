ffcb1ef88275385b73edc4262f4fdd4f3899407a
Stas Zinoviev <stas.zinoviev@brainbeanapps.com>
2019-12-02 17:04:44 +0200

    Add ImplementDataAccess project

---
+++ MS.Book.ImplementDataAccess/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MS.Book.ImplementDataAccess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task t = new Task(() => { });
+
+        }
+    }
+}
