1e90f1ad29485496024f1f101a794ca0229f0f99
Stas Zinoviev <stas.zinoviev@brainbeanapps.com>
2019-12-03 18:09:08 +0200

    Thread pool chapter

---
+++ MS.Book.ManageProgramFlow/ThreadPoolChapter/ThreadPoolChapterListing1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading;
+
+namespace MS.Book.ManageProgramFlow.ThreadPoolChapter
+{  
+    /// <summary>
+    /// Example work from thread pool
+    /// </summary>
+    public static class ThreadPoolChapterListing1
+    {
+        private static void Run()
+        {
+            ThreadPool.QueueUserWorkItem((s) =>
+            {
+                Console.WriteLine("Working on a thread from threadpool");
+            });
+            Console.ReadLine();
+        }
+    }
+}
