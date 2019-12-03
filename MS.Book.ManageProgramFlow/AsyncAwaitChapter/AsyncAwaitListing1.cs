8e8a075040873a186a145bb686fb7f743755f6d5
Stas Zinoviev <stas.zinoviev@brainbeanapps.com>
2019-12-03 18:10:04 +0200

    Async chapter

---
+++ MS.Book.ManageProgramFlow/AsyncAwaitChapter/AsyncAwaitListing1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MS.Book.ManageProgramFlow.AsyncAwaitChapter
+{
+    public static class AsyncAwaitListing1
+    {
+        public static void Run()
+        {
+            string result = DownloadContent().Result;
+            Console.WriteLine(result);
+        }
+
+        private static async Task<string> DownloadContent()
+        {
+            using(HttpClient client = new HttpClient())
+            {
+                string result = await client.GetStringAsync("http://www.microsoft.com");
+                return result;
+            }
+        }
+
+        
+        /// <summary>
+        /// Example working threads
+        /// </summary>
+        public static  void Test()
+        {
+            Task<int> t = new Task<int>(() =>
+            {
+                Console.WriteLine("Run task...");
+                Console.WriteLine();
+                Thread.Sleep(5000); 
+                Console.WriteLine("Task is completed");
+           
+                return 1;
+            });
+            t.Start();
+            Console.WriteLine("Run main..");
+            Thread.Sleep(2000);
+            Console.WriteLine("Main is completed..");
+           
+            
+            
+            
+            Console.WriteLine("Test");
+            
+            Console.WriteLine(t);
+        }
+    }
+}
---
+++ MS.Book.ManageProgramFlow/AsyncAwaitChapter/AsyncAwaitListing2.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Net.Http;
+using System.Text;
+
+namespace MS.Book.ManageProgramFlow.AsyncAwaitChapter
+{
+    public static class AsyncAwaitListing2
+    {
+        /// <summary>
+        /// Example disabling SynchronizationContext flow
+        /// Use when we don't need use async result in main thread (example UI thread in WPF/UWP)
+        /// </summary>
+        public static async void ButtonSimulate()
+        {
+            HttpClient httpClient = new HttpClient();
+
+            string content = await httpClient.GetStringAsync("http://www.microsoft.com")
+                .ConfigureAwait(false);
+            
+            
+            // If we set the ui element in this case, we will have exception
+            // textBox1.Text = content; => throw exception
+            
+            
+            // If we just write to the file, it's ok.
+            // We don't need necessary to write it synchronously
+            using (FileStream sourceStream = new FileStream("temp.html", FileMode.Create, FileAccess.Write, FileShare.None,4096, useAsync: true))
+            {
+                byte[] encodedText = Encoding.Unicode.GetBytes(content);
+                await sourceStream.WriteAsync(encodedText, 0, encodedText.Length)
+                    .ConfigureAwait(false);
+            };
+        }
+    }
+}
